c1a866529a284a350117e27721aaadf19f6167b0
**Anmol** <**ass53**@sfu.ca>
2024-06-04 05:58:38 -0700

    Updated indexjs to support mongdb

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
-const express = require("express")
+import express from "express";
+import cors from "cors";
+import records from "./routes/records.js";
 
-const PORT = process.env.PORT || 3001
+//support db integration
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-const app = express()
+app.use(cors());
+app.use(express.json());
+app.use("/record", records); //imported routes called here
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Connection to api working" });
-  });
+// app.get("/api", (req, res) => {
+//     res.json({ message: "Connection to api working" });
+//   });
 
+//start server
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`)
-})
+    console.log(`Server listening on ${PORT}`);
+});
